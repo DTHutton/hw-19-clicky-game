544f855fd50a65295241a4cbfba596c511df51d6
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-20 10:23:47 -0400

    moved components into folders to allow for css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Footer = props => {
+    return (
+        <div>
+            <footer className="page-footer">
+                <div className="footer-copyright">
+                    <div className="container">
+                        © Clicky Game by Derek Hutton
+            <a className="grey-text text-lighten-4 right" href="https://github.com/DTHutton/hw-19-clicky-game">GitHub Repo</a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
