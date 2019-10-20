b3d965b740088a092820ffe37d7c2f1e6d25ca7e
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-20 13:23:05 -0400

    added and populated clickChar.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar/Navbar"
-import ClickCards from "./components/ClickCards/ClickCards"
-import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar"
+import ClickCards from "./components/ClickCards"
+import Footer from "./components/Footer"
 
 function App() {
   return (
