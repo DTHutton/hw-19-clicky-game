a4c129c190e2aa115e02112f170ad183885b7203
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-22 19:44:52 -0400

    on click works and logs  successfully

--- src/App.js
+++ src/App.js
@@ -5,32 +5,15 @@ import CardWrapper from "./components/CardWrapper"
 import ClickCards from "./components/ClickCards"
 import Footer from "./components/Footer"
 import Wrapper from "./components/Wrapper"
-import pokemon from "./pokemon.json"
 
 class App extends Component {
-
-  state = {
-    pokemon
-  }
-
-  handleClick = props => {
-    console.log("click works")
-  }
 
   render() {
     return (
       <Wrapper>
         <Navbar />
         <CardWrapper>
-          {this.state.pokemon.map(pokemon => (
-            <ClickCards
-              onClick={this.handleClick}
-              id={pokemon.id}
-              key={pokemon.id}
-              name={pokemon.name}
-              image={pokemon.image}
-            />
-          ))}
+          <ClickCards />
         </CardWrapper>
         <Footer />
       </Wrapper>
--- src/components/ClickCards/index.js
+++ src/components/ClickCards/index.js
@@ -1,19 +1,37 @@
-import React from "react"
+import React, { Component } from "react"
 import "./style.css"
+import pokemon from "../../pokemon.json"
 
-const ClickCards = props => {
-    return (
-            <div className="card">
-                <div className="card-image">
-                    <img 
-                    alt={props.name} 
-                    src={props.image} 
-                    id={props.id}
-                    name={props.name}
-                    />
+class ClickCards extends Component {
+
+    state = {
+        pokemon
+    }
+
+    handleClick = event => {
+        this.setState({ pokemon })
+        console.log("click works")
+    }
+
+    render() {
+        return (
+            this.state.pokemon.map(({ id, name, image }) => (
+                <div className="card">
+                    <div className="card-image">
+                        <img
+                            alt={name}
+                            src={image}
+                            id={id}
+                            key={id}
+                            name={name}
+                            onClick={this.handleClick}
+                        />
+                    </div>
                 </div>
-            </div>
-    )
+            ))
+
+        )
+    }
 }
 
 export default ClickCards
